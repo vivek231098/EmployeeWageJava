29236e8e688d7ca9e6ed3486b2554b81cedfac42
vivek231098 <menonvivek196@gmail.com>
2020-08-19 12:30:09 +0530

    Interface added

---
+++ InEmployeeWageOops.java
@@ -0,0 +1,8 @@
+package test1;
+
+
+//Interface
+interface InEmployeeWageOops{
+	public void addCompanyEmpWage(String company,int empRatePerHr,int numberOfWorkingDay, int maxHrPerMonth );
+	public void computeEmpWage();
+}
